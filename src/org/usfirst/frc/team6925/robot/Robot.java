d7ce4946a4344b4aff8e223b9b15f0ddbf2f7b83
vhenrixon <vhenrixon@gmail.com>
2018-02-02 16:53:21 -0500

    joystick

--- src/org/usfirst/frc/team6925/robot/Robot.java
+++ src/org/usfirst/frc/team6925/robot/Robot.java
@@ -13,15 +13,15 @@ import org.usfirst.frc.team6925.robot.subsystems.DriveTrain;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.RobotDrive;
-import edu.wpi.first.wpilibj.SpeedControllerGroup;
+//import edu.wpi.first.wpilibj.RobotDrive;
+//import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
-import edu.wpi.first.wpilibj.VictorSP;
+//import edu.wpi.first.wpilibj.VictorSP;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj.command.Scheduler;
-import edu.wpi.first.wpilibj.command.Subsystem;
-import edu.wpi.first.wpilibj.livewindow.LiveWindow;;
+//import edu.wpi.first.wpilibj.command.Subsystem;
+//import edu.wpi.first.wpilibj.livewindow.LiveWindow;;
 
 /**
  * The VM is configured to automatically run this class, and to call the
@@ -33,7 +33,7 @@ import edu.wpi.first.wpilibj.livewindow.LiveWindow;;
 public class Robot extends IterativeRobot 
 {
 	
-	public Joystick controller = new Joystick (0);
+	public Joystick controller = new Joystick (1);
 	public int port;
 	public boolean triggerPressed = false;
 	public static OI oi;
@@ -190,7 +190,7 @@ public class Robot extends IterativeRobot
 	@Override
 	public void teleopInit()
 	{
-		controller = new Joystick(0);
+		//controller = new Joystick(1);
 	}
 		
 
