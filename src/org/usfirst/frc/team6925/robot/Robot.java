cbe33647ec5364513ac3f294f23282efeda00d2e
vhenrixon <vhenrixon@gmail.com>
2018-02-15 16:41:25 -0500

    controller added

--- src/org/usfirst/frc/team6925/robot/Robot.java
+++ src/org/usfirst/frc/team6925/robot/Robot.java
@@ -7,7 +7,9 @@
 
 package org.usfirst.frc.team6925.robot;
 
+import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.AnalogGyro;
+import edu.wpi.first.wpilibj.BuiltInAccelerometer;
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
@@ -16,10 +18,11 @@ import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.VictorSP;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.CameraServer;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.Encoder;
-
+import edu.wpi.first.wpilibj.GenericHID.Hand; 
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
@@ -47,8 +50,12 @@ public class Robot extends IterativeRobot
 			new VictorSP(RobotMap.m_rearRightMotor));
 	private DifferentialDrive m_myRobot = new DifferentialDrive(m_Left, m_Right);
 	private AnalogGyro m_gyro = new AnalogGyro(RobotMap.kGyroPort);
-	private Joystick m_joystick0 = new Joystick(RobotMap.joystick_port);
-	private Joystick m_joystick1 = new Joystick(RobotMap.joystick_port1);
+	private Joystick m_opJoyStick = new Joystick(RobotMap.joystick_port);
+	private XboxController m_driveController = new XboxController(RobotMap.m_driveController);
+	private BuiltInAccelerometer Acel = new BuiltInAccelerometer();
+	private Hand kLeft; 
+	private Hand kRight;
+
 	
 	/**
 	 * This function is run when the robot is first started up and should be
@@ -57,7 +64,9 @@ public class Robot extends IterativeRobot
 	@Override
 	public void robotInit()
 	{
-		CameraServer.getInstance().startAutomaticCapture();
+		UsbCamera m_frontCamera = CameraServer.getInstance().startAutomaticCapture(RobotMap.frontCamera);
+		m_frontCamera.setFPS(60);
+		m_frontCamera.setResolution(640, 640);
 		m_gyro.setSensitivity(RobotMap.kVoltsPerDegreePerSecond);
 		m_chooser.addDefault("Default Auto", kDefaultAuto);
 		m_chooser.addObject("My Auto", kCustomAuto);
@@ -110,15 +119,13 @@ public class Robot extends IterativeRobot
 	public void teleopPeriodic()
 	{
 		System.out.println("IN TELEOPPERIODIC");
-		System.out.println("VALUE OF Y = " + m_joystick0.getY());
-		System.out.println("The swing Value is = " + m_joystick0.getTwist());
 		double turningValue = (RobotMap.kAngleSetpoint - m_gyro.getAngle()) * RobotMap.kP;
+		double driveLeft =  Math.copySign(turningValue, m_driveController.getY(kLeft));// double check this 
+		double driveRight = Math.copySign(turningValue, m_driveController.getY(kRight));
 		// Invert the direction of the turn if we are going backwards
-		double throttle = (m_joystick0.getThrottle());
-		double turn = (m_joystick0.getTwist());
-		joyStick0 = Math.copySign(turningValue, m_joystick0.getY());
-		joyStick1 = Math.copySign(turningValue, m_joystick1.getY());
-		m_myRobot.tankDrive(joyStick0, joyStick1);
+		double actualSpeed = Acel.getY();
+		SmartDashboard.putNumber("Speed", actualSpeed);
+		m_myRobot.tankDrive(driveLeft, driveRight); 
 		
 		//Printing out Encorder "distance"
 		
--- src/org/usfirst/frc/team6925/robot/RobotMap.java
+++ src/org/usfirst/frc/team6925/robot/RobotMap.java
@@ -3,12 +3,14 @@ package org.usfirst.frc.team6925.robot;
 public class RobotMap {
 	//RobotMap is here just to allow to change variables easier 
 	public static int joystick_port = 0;
-	public static int joystick_port1 = 1;
+	public static int m_driveController = 1; 
 	public static int m_frontLeftMotor = 0;
 	public static int m_frontRightMotor = 1;
 	public static int m_rearLeftMotor = 2;
 	public static int m_rearRightMotor = 3;
-
+	public static int frontCamera = 0; 
+	public static int Left = 0; 
+	public static int Right = 1;
 	
 	
 	
