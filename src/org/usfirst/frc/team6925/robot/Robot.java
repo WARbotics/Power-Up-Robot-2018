cd28867b3f1c24f0c0e7d802e775daee6a35d963
dsgal <dsgal@DESKTOP-E8A82LN>
2018-04-05 08:39:32 -0400

    pushing

--- src/org/usfirst/frc/team6925/robot/Robot.java
+++ src/org/usfirst/frc/team6925/robot/Robot.java
@@ -292,7 +292,7 @@ public class Robot extends IterativeRobot
 	//Make sure when using this function, you add RAW SPEED value with no weight.
 	public double sinSmooth(double speed)
 	{
-		//
+		//l
 		//check out: y = .5 * Math.sin((Math.PI * x) - (Math.PI/2)) + .5
 		//^ is the best functiona
 		if (speed >= 0)
