cd4ac9426500a2f7b5571cf8d43d7f23b5e2af5e
Victor Henriksson <vhenrixon@gmail.com>
2018-09-13 16:57:06 -0400

    Clean
    Cleaned up old parts out of the code

--- src/org/usfirst/frc/team6925/robot/OI.java
+++ src/org/usfirst/frc/team6925/robot/OI.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package org.usfirst.frc.team6925.robot;
 
 //import
@@ -37,50 +36,3 @@ public class OI
 		
 	}
 }
-=======
-/*
- * This constructs and identifies the buttons located on the joystick
- */
-
-package org.usfirst.frc.team6925.robot;
-
-import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.buttons.Button;
-import edu.wpi.first.wpilibj.buttons.JoystickButton;
-import org.usfirst.frc.team6925.robot.RobotMap;
-
-
-public class OI 
-{
-	//Constructing the joystick and and placing it with it port number  
-	public Joystick driverJoystick = new Joystick(RobotMap.driverJoystick);
-	public Joystick operatorJoystick = new Joystick(RobotMap.operatorJoystick);
-	//Constructing the button for the Operator Joystick
-	Button basketActive;
-	Button basketReload;
-	Button inTakeButton;
-	Button outTakeButton;
-	Button testMotors;
-	Button testUnit;
-	Button reverseDriversControl;
-	public OI() 
-	{
-		//Constructing the button for the Operator Joystick
-		basketActive = new JoystickButton(operatorJoystick, 1);
-		basketReload = new JoystickButton(operatorJoystick, 2);
-		inTakeButton = new JoystickButton(operatorJoystick, 3);
-		outTakeButton = new JoystickButton(operatorJoystick, 4);
-		
-		
-		testMotors = new JoystickButton(operatorJoystick, 5);
-		testUnit = new JoystickButton(operatorJoystick,6);
-		reverseControl = new JoystickButton(driverJoystick, 5);
-		//placing buttons to there commands  
-		System.out.println("OI has been init");
-		
-		
-	}
-}
-
-
->>>>>>> master
